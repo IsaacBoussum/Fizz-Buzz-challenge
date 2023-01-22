d33617f99e993822a987fd76d5054a65440a830f
Isaac Boussum <isaacboussum10@gmail.com>
2023-01-22 14:40:52 -0700

    problem complete

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,1 +1,13 @@
 // Write your solution below this line:
+
+for (let i = 1; i < 50; i++){
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('fizzbuzz')
+    } else if (i % 3 === 0){
+        console.log('fizz')
+    } else if (i % 5 === 0){
+        console.log('buzz')
+    } else {
+        console.log(i)
+    }
+}
